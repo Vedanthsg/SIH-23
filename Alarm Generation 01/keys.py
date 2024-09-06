698ac3a1825e4d39fe3731140e0de54c8b8024b9
Vedanth <116838863+Vedanthsg@users.noreply.github.com>
2024-09-06 12:57:33 +0530

    updated files

--- Alarm Generation 01/keys.py
+++
@@ -1,4 +0,0 @@
-account_sid = 'ACbdc0a17d9e0f8c70e58632917b18851e'
-auth_token = '21c66361ddf354411cd58d662dc585f8'
-twilio_number = '+18886885126'
-target_number = '+917899159251'
--- Alarm Generation/keys.py
+++ Alarm Generation/keys.py
@@ -1,4 +1,6 @@
-account_sid = 'ACbdc0a17d9e0f8c70e58632917b18851e'
-auth_token = '21c66361ddf354411cd58d662dc585f8'
-twilio_number = '+18886885126'
-target_number = '+917899159251'
+# Twilio Keys 
+
+account_sid = ''
+auth_token = ''
+twilio_number = ''
+target_number = ''
